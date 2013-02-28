a74c4796f23d41c0a7ca8c2b24bcc6157f82be6a
Killdom <d.menard@live.com>
2013-02-28 18:59:01 -0500

    Modif ContraA

--- src/pack/contrat/ContraA.java
+++ src/pack/contrat/ContraA.java
@@ -10,4 +10,20 @@ package pack.contrat;
  */
 public class ContraA {
     
+    static double contratA(double montant, int numSoin) {
+        double montantRembourser = 0;
+        switch (Contrat.standardisationNumSoin(numSoin)) {
+            case 0:case 100:case 200:case 500:
+                montantRembourser = Contrat.calculRemboursement(montant,0.25);
+                break;
+            case 300:case 400:case 700:
+                montantRembourser = 0;
+                break;
+            case 600:
+                montantRembourser = montant * 0.4;
+                break;
+        }
+
+        return montantRembourser;
+    }
 }
