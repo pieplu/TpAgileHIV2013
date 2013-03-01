f08d64ed9cdcafdd09084d6bdb2d88dcc898c003
Killdom <d.menard@live.com>
2013-02-28 19:34:05 -0500

    Fini refactor ContratA

--- src/pack/contrat/ContraA.java
+++ src/pack/contrat/ContraA.java
@@ -10,20 +10,53 @@ package pack.contrat;
  */
 public class ContraA {
     
-    static double contratA(double montant, int numSoin) {
+    static double selectionSoinContratA(double montant, int numSoin) {
         double montantRembourser = 0;
-        switch (Contrat.standardisationNumSoin(numSoin)) {
-            case 0:case 100:case 200:case 500:
-                montantRembourser = Contrat.calculRemboursement(montant,0.25);
-                break;
-            case 300:case 400:case 700:
-                montantRembourser = 0;
-                break;
-            case 600:
-                montantRembourser = montant * 0.4;
-                break;
-        }
-
+        if(numSoin==0){calculRemboursementContratAnumSoin0 (montant,numSoin);}
+        if(numSoin==100){calculRemboursementContratAnumSoin100 (montant,numSoin);}
+        if(numSoin==200){calculRemboursementContratAnumSoin200 (montant,numSoin);}
+        if(numSoin==300){calculRemboursementContratAnumSoin300 (montant,numSoin);}
+        if(numSoin==400){calculRemboursementContratAnumSoin400 (montant,numSoin);}
+        if(numSoin==500){calculRemboursementContratAnumSoin500 (montant,numSoin);}
+        if(numSoin==600){calculRemboursementContratAnumSoin600 (montant,numSoin);}
+        if(numSoin==700){calculRemboursementContratAnumSoin700 (montant,numSoin);}
         return montantRembourser;
+    }
+    
+    static double calculRemboursementContratAnumSoin0 (double montant, int numSoin){
+        double montantRembourser;
+        return montantRembourser = Contrat.calculRemboursement(montant,0.25);
+    }
+    
+    static double calculRemboursementContratAnumSoin100 (double montant, int numSoin){
+        double montantRembourser;
+        return montantRembourser = Contrat.calculRemboursement(montant,0.25);
+    }
+    
+    static double calculRemboursementContratAnumSoin200 (double montant, int numSoin){
+        double montantRembourser;
+        return montantRembourser = Contrat.calculRemboursement(montant,0.25);
+    }
+    
+    static double calculRemboursementContratAnumSoin500 (double montant, int numSoin){
+        double montantRembourser;
+        return montantRembourser = Contrat.calculRemboursement(montant,0.25);
+    }
+    
+    static double calculRemboursementContratAnumSoin300 (double montant, int numSoin){
+        return 0;
+    }
+    
+    static double calculRemboursementContratAnumSoin400 (double montant, int numSoin){
+        return 0;
+    }
+    
+    static double calculRemboursementContratAnumSoin700 (double montant, int numSoin){
+        return 0;
+    }
+    
+    static double calculRemboursementContratAnumSoin600 (double montant, int numSoin){
+        double montantRembourser;
+        return montantRembourser = montant * 0.4;
     }
 }
