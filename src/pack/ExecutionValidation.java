3ea5b8b1cf3d60dbca5667cb6fff3dc7fd49ec40
mlatour <mathieu.latour1@gmail.com>
2013-03-10 14:38:21 -0400

    Il est maintenant possible de tester les documents XML.

--- src/pack/ExecutionValidation.java
+++ src/pack/ExecutionValidation.java
@@ -8,7 +8,6 @@ import pack.validation.Validation;
 import java.util.ArrayList;
 import org.w3c.dom.NodeList;
 
-
 /**
  *
  * @Mathieu Latour
@@ -34,82 +33,23 @@ public class ExecutionValidation {
         for (int i = 0; i < reclamationDuXML.getLength(); i++) {
             listeDesReclamationsDuClient.add(new ReclamationObject(reclamationDuXML, i, fichierAValider));
         }
-        
-        if (Validation.isClientNumberValid(clientID)) {
-            //System.out.println("Ça fonctionne");
-            //METHODE POUR FICHIER DEFECT.
-        }else{
-            //System.out.print("Le numero de client n'est pas valide");
-            testValid = false;
-        }
-        
-        if (Validation.isContractLetterValid(contratType)) {
-            //System.out.println("Ça fonctionne");
-            //METHODE POUR FICHIER DEFECT.
-        }else{
-            //System.out.print("La lettre du contrat n'est pas valide");
-            testValid = false;
-        }
-        
-        if (Validation.isActualMonthFormValid(mois)) {
-            //System.out.println("Ça fonctionne");
-            //METHODE POUR FICHIER DEFECT.
-        }else{
-            //System.out.print("Le mois n'est pas valide");
-            testValid = false;
-        }
 
-        for (int i = 0; i < listeDesReclamationsDuClient.size(); i++) {
-            //System.out.println("\n\n");
-            if (Validation.isAmountFormValid(listeDesReclamationsDuClient.get(i).getMontant())) {
-            //    System.out.println("La forme du montant en $ est valide");
-                
-            }else{
-            //    System.out.println("La forme du montant en $ n'est pas valide");
-                //METHODE DE FICHIER DEFECT.
-                testValid = false;
-            }
-            
-            if (Validation.dateFormatYearMonthDay((listeDesReclamationsDuClient.get(i).getDate()))){
-            //    System.out.println("La date est valide");
-                
-            }else{
-            //    System.out.println("La date n'est pas valide");
-                //METHODE DE FICHIER DEFECT.
-                testValid = false;
-            }
-            
-             if (Validation.dateFormatYearMonth(mois)){
-            //    System.out.println("La date est valide");
-                
-            }else{
-            //    System.out.println("La date n'est pas valide");
-                //METHODE DE FICHIER DEFECT.
-                testValid = false;
-            }
-            
-            if (Validation.isServiceNumberValid(listeDesReclamationsDuClient.get(i).getSoin())) {
-            //    System.out.println("Le numero de service est valide");
-            }else{
-            //    System.out.println("Le numero de service n'est pas valide");
-                //METHODE DE FICHIER DEFECT.
-                testValid = false;
-            }
-            
-            //System.out.println("Numero:"+listeDesReclamationsDuClient.get(i).getDate());
-            if (Validation.isDateValid(listeDesReclamationsDuClient.get(i).getDate(), mois)) {
-                
-            //    System.out.println("La date est valide");
-            }else{
-            //    System.out.println("La date n'est pas valide");
-                //METHODE DE FICHIER DEFECT.
-                testValid = false;
-                
-            //  System.out.println("\n");
-            }
-        }
 
-        //System.out.println(testValid);
-        return testValid;
+        boolean documentIsValid = true;
+        if (pack.validation.ClientID.isClientNumberValid(clientID)
+                && pack.validation.ContractLetter.isContractLetterValid(contratType)) {
+
+            for (int i = 0; i < listeDesReclamationsDuClient.size() && documentIsValid; i++) {
+                if (!pack.validation.Date.isDateValid(mois, listeDesReclamationsDuClient.get(i).getDate())
+                        || !pack.validation.ServiceNumber.isServiceNumberValid(listeDesReclamationsDuClient.get(i).getSoin())
+                        || !pack.validation.Amount.isAmountFormValid(listeDesReclamationsDuClient.get(i).getMontant())) {
+                    documentIsValid = false;
+                }
+            }
+        } else {
+            documentIsValid = false;
+        }
+        return documentIsValid;
+
     }
 }
