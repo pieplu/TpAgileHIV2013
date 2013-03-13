e4fda678211faa05a02555231995211831bcb8d5
unknown <hj991118@C32452.ens.uqam.ca>
2013-03-13 14:24:41 -0400

    ajout com override a B (oublie)

--- src/pack/contract/ContractB.java
+++ src/pack/contract/ContractB.java
@@ -12,42 +12,52 @@ public class ContractB extends ContractTemplate{
     
     
     
+    @Override
       double reimbursementCalculationForContractNumSoin0 (){
         return  Contract.reimbursementCalculation(0.5,40);
     }
     
+    @Override
       double reimbursementCalculationForContractNumSoin100 (){
         return  Contract.reimbursementCalculation(0.5,50);
     }
     
+    @Override
       double reimbursementCalculationForContractNumSoin150 (){
         return  Contract.reimbursementCalculation(0);
     }
     
+    @Override
       double reimbursementCalculationForContractNumSoin175 (){
         return  Contract.reimbursementCalculation(0.75);
     }
     
+    @Override
       double reimbursementCalculationForContractNumSoin200 (){
         return  Contract.reimbursementCalculation(1);
     }
     
+    @Override
       double reimbursementCalculationForContractNumSoin300 (){
         return  Contract.reimbursementCalculation(0.5);
     }
     
+    @Override
       double reimbursementCalculationForContractNumSoin400 (){
         return 0;
     }
     
+    @Override
       double reimbursementCalculationForContractNumSoin500 (){
         return  Contract.reimbursementCalculation(0.5,50);
     }
     
+    @Override
       double reimbursementCalculationForContractNumSoin600 (){
         return Contract.ammountInXmlFile;
     }
     
+    @Override
       double reimbursementCalculationForContractNumSoin700 (){
         return Contract.reimbursementCalculation(0.7);
     }
