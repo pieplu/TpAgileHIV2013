ea6e7bae00a9e26ab71457dfd6b8aba9733e8079
mlatour <mathieu.latour1@gmail.com>
2013-03-12 21:47:20 -0400

    Validation:
    - Vérifie si les tags essentiels sont présents (gère le NullPointerException)

---
+++ src/pack/validation/IsAXmlNodeMissing.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pack.validation;
+
+import pack.DocumentXml;
+import pack.ValidationExecution;
+
+/**
+ *
+ * @author utilisateur
+ */
+public class IsAXmlNodeMissing {
+    
+     static final String LIST_OF_XML_TAGS[] = new String[]{"reclamations","dossier","mois","soin","date","montant"};
+     static public final String TAG_ROOT = "reclamation";
+     
+     public static boolean isTagValid (DocumentXml tagToVerify,String expectedTagName) {
+        try{
+         tagToVerify.getNodesByName(expectedTagName);
+        }catch(NullPointerException e){
+            ValidationExecution.setErrorMessage("La balise " + expectedTagName + "n'est pas présente dans le document XML");
+            return false;
+        }
+        return true;
+        
+    }
+     
+    public static boolean isChildTagMissing (){
+        
+        return true;
+    }
+    
+}
