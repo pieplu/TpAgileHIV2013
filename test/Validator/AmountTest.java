39754644b7750486fb6d35b1dad27b10df586016
fe291196 <fe291196@C32701.ens.uqam.ca>
2013-03-24 12:15:13 -0430

    Renoamge de la classe Amount en Dollar et modification de la classe Dollar

--- test/Validator/AmountTest.java
+++ test/Validator/AmountTest.java
@@ -3,7 +3,7 @@
  * and open the template in the editor.
  */
 package Validator;
-import Validator.Amount;
+import Validator.Dollar;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
@@ -18,43 +18,43 @@ public class AmountTest {
 
     @Test
     public void containsNoDotForCents() {
-        assertFalse(Amount.containsDotForCents("1025$"));
+        assertFalse(Dollar.containsDotForCents("1025$"));
     }
 
     @Test
     public void amountFormatIsValid() {
-        assertTrue(Amount.isAmountFormValid("1000.25$"));
+        assertTrue(Dollar.isAmountFormValid("1000.25$"));
     }
 
     @Test
     public void amountFormatIsInvalidMissingDot() {
-        assertFalse(Amount.isAmountFormValid("1025$"));
+        assertFalse(Dollar.isAmountFormValid("1025$"));
     }
     
     @Test
     public void amountFormatWithComma() {
-        assertTrue(Amount.isAmountFormValid("1025,01$"));
+        assertTrue(Dollar.isAmountFormValid("1025,01$"));
     }
 
     
 
     @Test
     public void amountFormatIsInvalidLengthIsTooSmall() {
-        assertFalse(Amount.isAmountFormValid(".00$"));
+        assertFalse(Dollar.isAmountFormValid(".00$"));
     }
 
     @Test
     public void amountFormatIsInvalidMissingDollarSign() {
-        assertFalse(Amount.isAmountFormValid("1025.00"));
+        assertFalse(Dollar.isAmountFormValid("1025.00"));
     }
 
     @Test
     public void amountFormatIsInvalidMissingOneDigitForCents() {
-        assertFalse(Amount.isAmountFormValid("1025.0$"));
+        assertFalse(Dollar.isAmountFormValid("1025.0$"));
     }
 
     @Test
     public void amountFormatIsInvalidOneDigitIsAChar() {
-        assertFalse(Amount.isAmountFormValid("10a5.0$"));
+        assertFalse(Dollar.isAmountFormValid("10a5.0$"));
     }
 }
