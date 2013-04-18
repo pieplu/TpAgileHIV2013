2438a0e92906f822c19085d6619cffb1a133eda0
Pieplu <alexis.pieplu@gmail.com>
2013-04-17 22:59:26 -0400

    71

--- src/test/java/RefundCalculator/CalculatorTest.java
+++ src/test/java/RefundCalculator/CalculatorTest.java
@@ -36,7 +36,63 @@ public class CalculatorTest {
     public void testRefundCalculator_int2() {
         client = new FamilyMemberData("700", "0.00$", "A", "A");
         mock.refundCalculator(client);
-        assertEquals(mock.refundCalculator(632), 0);
+        assertEquals(mock.refundCalculator(25), 0);
+    }
+    
+     @Test
+    public void testRefundCalculator_int3() {
+        client = new FamilyMemberData("0", "100.00$", "A", "H1");
+        mock.refundCalculator(client);
+        assertEquals(mock.refundCalculator(25), 1250);
+    }
+     
+    @Test
+    public void testRefundCalculator_int4() {
+        client = new FamilyMemberData("0", "100.00$", "A", "A");
+        mock.refundCalculator(client);
+        assertEquals(mock.refundCalculator(25), 2500);
+    }
+    
+    @Test
+    public void testRefundCalculator_int5() {
+        client = new FamilyMemberData("0", "100.00$", "A", "C1");
+        mock.refundCalculator(client);
+        assertEquals(mock.refundCalculator(25), 2500);
+    }
+    
+    @Test
+    public void testRefundCalculator_int6() {
+        client = new FamilyMemberData("0", "100.00$", "B", "B");
+        mock.refundCalculator(client);
+        assertEquals(mock.refundCalculator(25), 2500);
+    }
+    
+    @Test
+    public void testRefundCalculator_int7() {
+        client = new FamilyMemberData("0", "100.00$", "B", "H1");
+        mock.refundCalculator(client);
+        assertEquals(mock.refundCalculator(25), 1250);
+    }
+    
+    @Test
+    public void testRefundCalculator_int8() {
+        client = new FamilyMemberData("0", "100.00$", "B", "H2");
+        mock.refundCalculator(client);
+        assertEquals(mock.refundCalculator(25), 1250);
+    }
+    
+    @Test
+    public void testRefundCalculator_int9() {
+        client = new FamilyMemberData("0", "100.00$", "C", "E1");
+        mock.refundCalculator(client);
+        assertEquals(mock.refundCalculator(90), 9000);
+    }
+    
+    @Test
+    public void testRefundCalculator_int10() {
+        client = new FamilyMemberData("150", "100.00$", "C", "C");
+        mock.refundCalculator(client);
+        assertEquals(mock.refundCalculator(75), 7500);
     }
     
     @Test
